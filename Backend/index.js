a861c72a4817ea2ef08bb42beffca574f54450a1
RojenHasan <rojen.hasan@student.ucll.be>
2024-04-21 17:43:10 +0200

    connect to db mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(()=> console.log("db connected")).catch((err)=> console.log(err));
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
